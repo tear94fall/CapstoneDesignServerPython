63f8b003a5129751a20db232bb268c6be13fb10d
tear94fall <tear94fall@naver.com>
2019-03-06 21:18:47 +0900

    server log file add to the repository

--- server/async_file_server.py
+++ server/async_file_server.py
@@ -1,51 +1,49 @@
 import asyncio
 from os.path import exists
 
-HOST = ''
-PORT = 9009
+
+async def handle_echo(reader, writer):
+
+    filename = await reader.read(1024)
+    filename = filename.decode()
+    data_transferred = 0
+
+    print("Received File Name %r" % filename)
+
+    if not exists(filename):  # 파일이 해당 디렉터리에 존재하지 않으면
+        print("file is not exist")
+        return  # handle()함수를 빠져 나온다.
+
+    print('파일[%s] 전송 시작...' % filename)
+    with open(filename, 'rb') as f:
+        try:
+            data = f.read(1024)  # 파일을 1024바이트 읽음
+            while data:  # 파일이 빈 문자열일때까지 반복
+                writer.write(data)
+                data_transferred += len(data)
+                data = f.read(1024)
+        except Exception as e:
+            print(e)
+
+    print('전송완료[%s], 전송량[%d]' % (filename, data_transferred))
+    await writer.drain()
+
+    print("Close the client socket")
+    writer.close()
 
 
-class MyTcpHandler:
-    def handle(self):
-        event_loop = asyncio.get_event_loop()
-        client_addr = ''
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
+server = loop.run_until_complete(coro)
 
-        async_start = asyncio.start_server(self.io_handle, self.address, self.port, loop=self.event_loop)
-        server = self.event_loop.run_until_complete(async_start)
-        addr = server.sockets[0].getsockname()
-        a_log('서버 서비스 시작 중 %s ' % str(addr), L_CRITICAL_EVENT)
-        self.event_loop.run_forever()
+# Serve requests until Ctrl+C is pressed
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
 
-        data_transferred = 0
-        print('[%s] 연결됨' % self.client_address[0])
-        filename = self.request.recv(1024)  # 클라이언트로 부터 파일이름을 전달받음
-        filename = filename.decode()  # 파일이름 이진 바이트 스트림 데이터를 일반 문자열로 변환
-
-        if not exists(filename):  # 파일이 해당 디렉터리에 존재하지 않으면
-            return  # handle()함수를 빠져 나온다.
-
-        print('파일[%s] 전송 시작...' % filename)
-        with open(filename, 'rb') as f:
-            try:
-                data = f.read(1024)  # 파일을 1024바이트 읽음
-                while data:  # 파일이 빈 문자열일때까지 반복
-                    data_transferred += self.request.send(data)
-                    data = f.read(1024)
-            except Exception as e:
-                print(e)
-
-        print('전송완료[%s], 전송량[%d]' % (filename, data_transferred))
-
-
-def runServer():
-    print('++++++파일 서버를 시작++++++')
-    print("+++파일 서버를 끝내려면 'Ctrl + C'를 누르세요.")
-
-    try:
-        server = socketserver.TCPServer((HOST, PORT), MyTcpHandler)
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print('++++++파일 서버를 종료합니다.++++++')
-
-
-runServer()
+# Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
--- server/async_ftp_client.py
+++ server/async_ftp_client.py
@@ -1,33 +1,41 @@
 import asyncio
 
 
-HOST = 'localhost'
-PORT = 9009
+async def tcp_echo_client(filename, loop):
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
+                                                   loop=loop)
 
+    writer.write(filename.encode())
 
-def getFileFromServer(filename):
+    data = await reader.read(100)
+    print('Received: %r' % data.decode())
+
+    print('Close the socket')
+    writer.close()
+
+    # =================================== #
     data_transferred = 0
+    data = await reader.read(1024)
+    if not data:
+        print('파일[%s]: 서버에 존재하지 않거나 전송중 오류발생' % filename)
+        return
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((HOST, PORT))
-        sock.sendall(filename.encode())
+    with open('download/' + filename, 'wb') as f:
+        try:
+            while data:
+                f.write(data)
+                data_transferred += len(data)
+                data = await reader.read(1024)
+        except Exception as e:
+            print(e)
 
-        data = sock.recv(1024)
-        if not data:
-            print('파일[%s]: 서버에 존재하지 않거나 전송중 오류발생' % filename)
-            return
-
-        with open('download/' + filename, 'wb') as f:
-            try:
-                while data:
-                    f.write(data)
-                    data_transferred += len(data)
-                    data = sock.recv(1024)
-            except Exception as e:
-                print(e)
-
-    print('파일[%s] 전송종료. 전송량 [%d]' % (filename, data_transferred))
+        print('파일[%s] 전송종료. 전송량 [%d]' % (filename, data_transferred))
 
 
+
+
+message = 'Hello World!'
 filename = input('다운로드 받은 파일이름을 입력하세요:')
-getFileFromServer(filename)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(tcp_echo_client(filename, loop))
+loop.close()
