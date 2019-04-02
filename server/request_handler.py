a12c4d3ecea02300a413e03d1990c31add8a21c4
tear94fall <tear94fall@naver.com>
2019-04-02 12:35:41 +0900

    select 쿼리문을 이용한 요청 처리 객체 수정

--- server/request_handler.py
+++ server/request_handler.py
@@ -1,8 +1,53 @@
-from server.async_server import *
-from server.server_logger import a_log
-from asyncio import StreamReader as reader, StreamWriter as writer
+import hashlib
+from server.async_database import *
+from asyncio import StreamReader, StreamWriter
+from server.async_database import *
 
 
-class request_handler:
+class RequestHandler:
     def __init__(self):
-        pass
+        self.request = []
+
+    async def Request_Binding(self, request_number):
+        # 요청 넘버는 2의 배수로 정한다
+        if request_number == 2:
+            echo = EchoRequest()
+            result = await echo.main()
+        elif request_number == 4:
+            select = SelectRequest()
+            result = await select.main()
+        elif request_number == 6:
+            pass
+        elif request_number == 8:
+            pass
+
+        return result
+
+
+class EchoRequest(RequestHandler):
+    def __init__(self):
+        super().__init__()
+
+    async def main(self):
+        test_query2 = "SELECT student_name from students"
+        result = await query_operator(test_query2)
+        return result
+
+
+class SelectRequest(RequestHandler):
+    def __init__(self):
+        super().__init__()
+
+    async def main(self):
+        test_query2 = "SELECT insert_date from students"
+        result = await query_operator(test_query2)
+        return result
+
+
+class CreateTableRequest(RequestHandler):
+    def __init__(self):
+        super().__init__()
+
+    async def main(self):
+        result = None
+        return result
