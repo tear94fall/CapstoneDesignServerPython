56f2b3aa740a536f7891d3e634647bc03b0bd1e7
tear94fall <tear94fall@naver.com>
2019-04-15 22:38:01 +0900

    1.데이터 버퍼 클래스 추가 2.데이터 암호화 추가

--- server/data_buffer.py
+++ server/data_buffer.py
@@ -1,5 +1,29 @@
+import hashlib
 
 
 class DataBuffer:
     def __init__(self):
-        pass
+        self.data_buffer = None
+
+    def set_data(self, data: str):
+        self.data_buffer = data
+
+    def get_data(self):
+        return self.data_buffer
+
+    def crypt_data(self):
+        self.data_buffer = self.data_buffer.encode('utf-8')
+        sha256 = hashlib.sha256()
+        sha256.update(self.data_buffer)
+        self.data_buffer = sha256.hexdigest()
+
+    def valid_check(sel)->bool:
+
+        return True
+
+
+if __name__ == "__main__":
+    buffer = DataBuffer()
+    buffer.set_data("123123123123")
+    buffer.crypt_data()
+    print("=== end ===")
