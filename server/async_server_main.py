4aa94768e5651c6ea966d8155f3dfb12dda75518
tear94fall <tear94fall@naver.com>
2019-05-25 19:06:34 +0900

    외부에서 접속 테스트를 위해 아이피 부분 로직 수정함.

--- server/async_server_main.py
+++ server/async_server_main.py
@@ -18,9 +18,10 @@ if __name__ == '__main__':
     description = "서버 로그" + datetime.datetime.now().strftime(" %Y-%m-%d %H:%M:%S") + " >> 서버 작동 시작"
     print(description)
 
+    # 아이피는 기본 값 0.0.0.0 으로 한다. 로컬 호스트에서 테스트시에 127.0.0.1로 사용함
     args = ArgsConfig.instance()
     args.add_argument('--port', type=int, default=8888)
-    args.add_argument('--ip', type=str, default='127.0.0.1')
+    args.add_argument('--ip', type=str, default='0.0.0.0')
     args.add_argument('--verbose', type=int, default=3)
 
     config = args.parse_args()
